c08bc8bcf9f8d9f808aa277a4c7d5e47711a5649
Rodrigo Lúcio <luciodigo@gmail.com>
2021-08-31 23:14:31 -0300

    Criação de entidades

---
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,54 @@
+package br.com.alura.loja.modelo;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private LocalDate dataCadastro = LocalDate.now();
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Cliente cliente;
+	
+	private Double valorTotal;
+
+	public Pedido(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+}
