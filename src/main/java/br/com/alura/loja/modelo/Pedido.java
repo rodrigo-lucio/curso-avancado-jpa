93b59944cc4ebd01e15da9c2c3407560fdbc8b3f
Rodrigo Lúcio <luciodigo@gmail.com>
2021-08-31 23:28:04 -0300

    Criado entidades de itens do pedido e seus relacionamentos

--- src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -1,6 +1,8 @@
 package br.com.alura.loja.modelo;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,8 +25,14 @@ public class Pedido {
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
 	
-	private Double valorTotal;
+	@OneToMany
+	private List<ItemPedido> itensPedido;
+	
+	private BigDecimal valorTotal;
 
+	public Pedido() {
+	}
+		
 	public Pedido(Cliente cliente) {
 		this.cliente = cliente;
 	}
@@ -44,11 +53,11 @@ public class Pedido {
 		this.cliente = cliente;
 	}
 
-	public Double getValorTotal() {
+	public BigDecimal getValorTotal() {
 		return valorTotal;
 	}
 
-	public void setValorTotal(Double valorTotal) {
+	public void setValorTotal(BigDecimal valorTotal) {
 		this.valorTotal = valorTotal;
 	}
 }
